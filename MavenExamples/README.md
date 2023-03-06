ce6c0beb30a51ac965590eacd16de2217e98a504
Pavel Akhtyamov <akhtyamovpavel@gmail.com>
2023-03-07 00:44:59 +0300

    Added 00 Maven Example

---
+++ MavenExamples/README.md
@@ -0,0 +1,7 @@
+## Two main classes to execute
+
+Examples
+```shell
+mvn exec:java@ClassA
+mvn exec:java@ClassB
+```
---
+++ MavenExamples/src/main/java/MainClassA.java
@@ -0,0 +1,5 @@
+public class MainClassA {
+    public static void main(String[] args) {
+        System.out.println("Hello world from A");
+    }
+}
